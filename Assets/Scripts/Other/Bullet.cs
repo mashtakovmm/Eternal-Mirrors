4c989cfc7c9416d198417c029e295e94dadc31e6
Max <48150531+mashtakovmm@users.noreply.github.com>
2024-03-27 22:38:34 +0300

    add sprites and animations

---
+++ Assets/Scripts/Other/Bullet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [HideInInspector] public float speed;
+    [HideInInspector] public int damage;
+    [SerializeField] private bool isPlayerBullet;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 1.22f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isPlayerBullet && other.transform.tag != "Player" && other.transform.tag != "Plane")
+        {
+            Debug.Log("hit");
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerContorller.cs
+++ Assets/Scripts/Player/PlayerContorller.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PlayerContorller : MonoBehaviour
@@ -12,18 +11,18 @@ public class PlayerContorller : MonoBehaviour
     [Header("Stats")]
     [SerializeField] private int _health;
 
-    private List<IInteractable> interactables; 
-    private CircleCollider2D circleCollider;
-
-
+    private List<IInteractable> interactables;
+    private Animator animator;
     private Vector2 move;
-
+    SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         interactables = new List<IInteractable>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -43,6 +42,7 @@ public class PlayerContorller : MonoBehaviour
     private void Move()
     {
         rb.velocity = move * speed;
+        animator.SetBool("isRunning", rb.velocity != Vector2.zero);
     }
 
     private void OnEnable()
@@ -68,6 +68,10 @@ public class PlayerContorller : MonoBehaviour
     private void HandleMoveEvent(Vector2 vector2)
     {
         move = vector2;
+        if (move != Vector2.zero)
+        {
+            spriteRenderer.flipX = move.x < 0;
+        }
     }
 
     public void TakeDamage(int value)
